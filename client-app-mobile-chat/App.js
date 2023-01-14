2e8dc503c81be1d7e118dd6888fbddcd7fad175d
criss-brierre <crissbrierre1303@gmail.com>
2023-01-14 15:56:25 +0100

    homepage finished

--- client-app-mobile-chat/App.js
+++ client-app-mobile-chat/App.js
@@ -4,11 +4,12 @@ import AuthComponent from './components/AuthComponent/auth.component.jsx'
 
 import RegisterComponent from './components/RegisterComponent/register.component.jsx'
 
+import HomeComponent from './components/HomeComponent/home.component.jsx'
+
 export default function App() {
 
   return (
-  <RegisterComponent/>
-  );
+    <HomeComponent />  );
   
 }
 
