6add1d03b286b985333b13a65d448ea787737c04
basil-collette <basil.collette@laplateforme.io>
2023-01-20 11:52:22 +0100

    ok

--- api-app-mobile-chat/server.js
+++ api-app-mobile-chat/server.js
@@ -28,7 +28,6 @@ server.listen(port, /*hostname,*/ () => {
     //console.log(`Server running at http://${hostname}:${port}/...`);
     console.log(`Server running on port :${port}...`);
 });
-server.on('error', onError);
 
 //SOCKET __________________________________________________________________ SOCKET
 
@@ -61,7 +60,6 @@ app.use('/admin', require('./middlewares/UserMiddlewares').isAdmin, require('./r
 
 // error handler
 app.use((err, req, res, next) => {
-    console.log(err);
     if (res.headersSent) {
         return next(err);
     }
@@ -88,34 +86,4 @@ function normalizePort(val) {
     }
 
     return false;
-}
-
-// Event listener for HTTP server "error" event.
-function onError(error) {
-    console.error('error function : ');
-    console.error(error);
-    
-    throw new Error('test error');
-
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
 }
